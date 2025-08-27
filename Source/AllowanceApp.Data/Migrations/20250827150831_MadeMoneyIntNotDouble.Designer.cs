18b049a550828b9ac6f287880a623ecad6822c52
verySpicyHeatWave <bncobb1989@gmail.com>
2025-08-27 08:16:02 -0700

    Updated database schema with new money storage method

--- Source/AllowanceApp.Data/Migrations/20250827150831_MadeMoneyIntNotDouble.Designer.cs
+++ Source/AllowanceApp.Data/Migrations/20250827150831_MadeMoneyIntNotDouble.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using AllowanceApp.Data.Contexts;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -10,9 +11,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace AllowanceApp.Data.Migrations
 {
     [DbContext(typeof(AccountContext))]
-    partial class AccountContextModelSnapshot : ModelSnapshot
+    [Migration("20250827150831_MadeMoneyIntNotDouble")]
+    partial class MadeMoneyIntNotDouble
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder.HasAnnotation("ProductVersion", "9.0.8");
---
+++ Source/AllowanceApp.Data/Migrations/20250827150831_MadeMoneyIntNotDouble.cs
@@ -0,0 +1,66 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace AllowanceApp.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class MadeMoneyIntNotDouble : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<int>(
+                name: "Amount",
+                table: "Transactions",
+                type: "INTEGER",
+                nullable: false,
+                oldClrType: typeof(double),
+                oldType: "REAL");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "Price",
+                table: "AllowancePoints",
+                type: "INTEGER",
+                nullable: false,
+                oldClrType: typeof(double),
+                oldType: "REAL");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "Balance",
+                table: "Accounts",
+                type: "INTEGER",
+                nullable: false,
+                oldClrType: typeof(double),
+                oldType: "REAL");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<double>(
+                name: "Amount",
+                table: "Transactions",
+                type: "REAL",
+                nullable: false,
+                oldClrType: typeof(int),
+                oldType: "INTEGER");
+
+            migrationBuilder.AlterColumn<double>(
+                name: "Price",
+                table: "AllowancePoints",
+                type: "REAL",
+                nullable: false,
+                oldClrType: typeof(int),
+                oldType: "INTEGER");
+
+            migrationBuilder.AlterColumn<double>(
+                name: "Balance",
+                table: "Accounts",
+                type: "REAL",
+                nullable: false,
+                oldClrType: typeof(int),
+                oldType: "INTEGER");
+        }
+    }
+}
